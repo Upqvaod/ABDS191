dd895b5904aa4ac91bb5b0012fdd2f88544117cb
DiegoV.O <122044299@upq.edu.mx>
2024-06-03 18:27:26 -0600

    Update ExamenJoins.sql

--- Examen/ExamenJoins.sql
+++ Examen/ExamenJoins.sql
@@ -12,7 +12,29 @@ SELECT * FROM Comics;
 
 SELECT * FROM Autores;
 
+SELECT * FROM Comic_Compras;
+
 INSERT INTO Autores(nombre, pais_origen) VALUES 
 
-('Robert Kirkman', 'USA',),
-('Allan Moore', 'Canada'
+('Robert Kirkman', 'USA'),
+('Allan Moore', 'Canada'),
+('Stan Lee', 'USA'),
+('Pepito', 'Mexico');
+
+INSERT INTO Comics(id_autor) VALUES 
+(300),(301),(302),(303),(300),(301),(302),(303),(300),(301),(302) ;
+
+
+SELECT c.nombre as 'Nombre del comic', a.nombre as 'Autor', a.pais_origen as 'Pais de Origen'
+FROM Comics c
+LEFT JOIN Autores a ON c.id_autor = a.id_autor
+
+
+SELECT a.nombre, c.cantidad
+FROM Autores a
+JOIN Comics co ON a.id_autor = co.id_autor
+JOIN comic_compras c on c.id_comic = co.id_comic
+
+
+SELECT * FROM Comic_Compras;
+
